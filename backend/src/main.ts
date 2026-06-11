cff08ad0ad0888c343d178a0eb792c235cf647ce
Paulo Vitor Gomes <bpaulovitorgomes@gmail.com>
2026-06-11 15:06:56 -0300

    Clean

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,16 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Habilita a validação global com base nos DTOs
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Remove campos que não estão no DTO
-    forbidNonWhitelisted: true, // Retorna erro se enviar campo não esperado
-    transform: true, // Transforma os dados recebidos para os tipos do DTO
+    whitelist: true, 
+    forbidNonWhitelisted: true, 
+    transform: true, 
   }));
   
-  // Habilita o CORS para o frontend (porta 3001)
   app.enableCors({
-    origin: '*', // Para fins de desenvolvimento
+    origin: '*', 
   });
 
   await app.listen(3000);
